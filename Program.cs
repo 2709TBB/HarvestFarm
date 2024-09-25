3d3a4d4be06f557f32250a66b39e5b66cfa946ed
Thịnh Trần Phạm Quang <Thinhdragon2307@gmail.com>
2024-09-26 00:15:42 +0700

    fix caculate profit, cost

--- Program.cs
+++ Program.cs
@@ -1,17 +1,21 @@
 ﻿using HarvestFarm;
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     public static void Main()
     {
+        
         Player player = new Player("TBB", 100);
 
-        List<Product> products = new List<Product>();
-
-        products.Add(new Wheat(30, 50, 3, 5, 5));
-        products.Add(new Tomato(40, 90, 4, 6, 7));
-        products.Add(new Sunflower(20, 60, 2, 4, 3));
+        
+        List<Product> products = new List<Product>
+        {
+            new Wheat(30, 50, 3, 5, 5),    
+            new Tomato(40, 90, 4, 6, 7),   
+            new Sunflower(20, 60, 2, 4, 3) 
+        };
 
         List<Product> chosenProducts = new List<Product>();
 
@@ -22,12 +26,15 @@ class Program
         }
 
         int totalCost = 0;
+
+        
         while (true)
         {
             Console.WriteLine("Choose the plant you want from 1 to 3");
             string input = Console.ReadLine();
             int choice;
             bool validChoice = int.TryParse(input, out choice);
+
             if (!validChoice || choice < 0 || choice > products.Count)
             {
                 Console.WriteLine("Choose again!");
@@ -39,51 +46,61 @@ class Program
             }
 
             Product selectedProduct = products[choice - 1];
-            if (totalCost + selectedProduct.Cost > player.Reward)
+
+            
+            int productTotalCost = selectedProduct.Cost + selectedProduct.FertilizerCost + selectedProduct.WaterCost;
+
+            
+            if (totalCost + productTotalCost > player.Reward)
             {
                 Console.WriteLine("Not enough points to buy this");
                 continue;
             }
+
+            
             chosenProducts.Add(selectedProduct);
-            totalCost += selectedProduct.Cost;
+            totalCost += productTotalCost;
 
             Console.WriteLine($"{selectedProduct.GetType().Name} has been added to your inventory.");
-
-            Console.WriteLine("\nList of your choice");           
+            Console.WriteLine("\nList of your choice");
             foreach (Product product in chosenProducts)
             {
                 Console.WriteLine(product.GetType().Name);
             }
         }
 
+        
         if (totalCost > 0)
         {
-            player.DeductReward(totalCost);
+            player.DeductReward(totalCost); 
             Console.WriteLine($"You have spent {totalCost} points. Your remaining points: {player.Reward}");
         }
         else
         {
             Console.WriteLine("You haven't chosen any products.");
+            return; 
         }
 
-
         
-
-        //Planting process
-        for (int i = 0; i < chosenProducts.Count; i++)
+        foreach (Product product in chosenProducts)
         {
-            chosenProducts[i].Seed();
-           
-            if (chosenProducts[i] is ICare careableProduct)
+            product.Seed();
+
+            
+            if (product is ICare careableProduct)
             {
                 careableProduct.Feed();
                 careableProduct.ProvideWater();
             }
 
-            chosenProducts[i].Harvest();
-            int profit = chosenProducts[i].Value - chosenProducts[i].TotalCost;
-            Console.WriteLine("Profit: " +profit);
+            product.Harvest();
+
+            
+            int profit = product.Value; 
             player.AddReward(profit);
+            Console.WriteLine($"You earned {profit} points from {product.GetType().Name}. Total points: {player.Reward}");
         }
+
+        Console.WriteLine($"Final total points: {player.Reward}");
     }
 }
